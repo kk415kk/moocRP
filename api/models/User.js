51f333ce7ffe56c8e4e837eb6b207212b1f55360
Kevin <kk415kk@gmail.com>
2014-03-07 14:17:08 -0800

    added bcrypt pw encryption

--- api/models/User.js
+++ api/models/User.js
@@ -35,25 +35,35 @@ module.exports = {
     },
 
     // Instance methods
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      delete obj.confirmation;
+      delete obj._csrf;
+      return obj;
+    }, 
+  },
 
-    // 'beforeCreate' is a lifecycle callback; essentially, these are hooks that we
-    // can tape into to change data
-    // See Sails documentation on Models for more info/examples
-    beforeCreate: function (attrs, next) {
-      var bcrypt = require('bcrypt');
+  // 'beforeCreate' is a lifecycle callback; essentially, these are hooks that we
+  // can tape into to change data
+  // See Sails documentation on Models for more info/examples
+  beforeCreate: function (values, next) {
+    var bcrypt = require('bcrypt');
 
-      bcrypt.genSalt(10, function(err, salt) {
-        if (err) return next(err);
-
-        bcrypt.hash(attrs.password, salt, function(err, hash) {
-          if (err) return next(err);
-
-          attrs.password = hash;
-          next();
-        });
-      });
+    if (!values.password || values.password != values.confirmation) {
+      return next({err: ["Password doesn't match password confirmation."]});
     }
 
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
 
+      bcrypt.hash(values.password, salt, function(err, encrypted) {
+        if (err) return next(err);
+
+        values.password = encrypted;
+        values.online = true;
+        next();
+      });
+    });
   }
 };
