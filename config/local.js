5a139bb0683f2391c2701d2339c606c211530f9a
Kevin <kk415kk@gmail.com>
2014-03-11 02:19:55 -0700

    disabled ssl temp

--- config/local.js
+++ config/local.js
@@ -1,23 +1,23 @@
-var fs = require('fs');
+// var fs = require('fs');
  
-module.exports = {
+// module.exports = {
  
-  port: process.env.PORT || 1337,
-  environment: process.env.NODE_ENV || 'development',
+//   port: process.env.PORT || 1337,
+//   environment: process.env.NODE_ENV || 'development',
  
-  express: { serverOptions : {
-    key: fs.readFileSync('ssl/key.pem'),
-    cert: fs.readFileSync('ssl/cert.pem')
-  }}
+//   express: { serverOptions : {
+//     key: fs.readFileSync('ssl/key.pem'),
+//     cert: fs.readFileSync('ssl/cert.pem')
+//   }}
 
-  //NEED SUDO TO RUN ON PORT 443
-  // express: { serverOptions : {
-  //     key: fs.readFileSync('ssl/ia.key'),
-  //     cert: fs.readFileSync('ssl/ia.crt')
-  //   }
-  // },
-  // port: process.env.PORT || 443,
-  // environment: process.env.NODE_ENV || 'development'
+//   NEED SUDO TO RUN ON PORT 443
+//   express: { serverOptions : {
+//       key: fs.readFileSync('ssl/ia.key'),
+//       cert: fs.readFileSync('ssl/ia.crt')
+//     }
+//   },
+//   port: process.env.PORT || 443,
+//   environment: process.env.NODE_ENV || 'development'
 
  
-};
+// };
