4bf166755732f3e0472b90ef121fd1f151f077da
Kevin <kk415kk@gmail.com>
2014-08-09 21:33:20 -0700

    Created setup bash script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,14 +1,18 @@
-mkdir ../../moocRP_base
-mv ../../moocRP moocRP_base
-mv ../../moocRP_base moocRP
-mkdir ../../datasets
-mkdir ../../datasets/encrypted
-mkdir ../../datasets/extracted
-mkdir ../../datasets/non_pii
-mkdir ../../datasets/pii
-mkdir ../../visualizations
-mkdir ../../visualizations/tmp
-mkdir ../../visualizations/archives
-mkdir ../../moocRP/logs
-touch ../../moocRP/logs/production.log
-touch ../../moocRP/logs/development.log
+##########################
+# Author: Kevin Kao      #
+##########################
+#!/bin/bash
+
+echo "Welcome to moocRP setup. Creating file directory structure..."
+# Grab this script's absolute filepath to its directory
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DATASET_FOLDERS=( "encrypted" "extracted" "non_pii" "pii" )
+VISUAL_FOLDERS=(tmp archives)
+LOG_FILES=(production.log development.log)
+
+for folder in "${DATASET_FOLDERS[@]}"; do CREATE="$SCRIPT_PATH/../../datasets/${folder}"; echo "Creating $CREATE"; mkdir -p "$CREATE"; done
+for folder in "${VISUAL_FOLDERS[@]}"; do CREATE="$SCRIPT_PATH/../../visualizations/${folder}"; echo "Creating $CREATE"; mkdir -p "$CREATE"; done
+
+mkdir -p "$SCRIPT_PATH/../logs"
+for files in "${LOG_FILES[@]}"; do CREATE="$SCRIPT_PATH/../logs/${files}"; "Creating $CREATE"; touch "$CREATE"; done
