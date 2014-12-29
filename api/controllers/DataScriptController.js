d97431c6d28a440a0c48fc04cbcf0017b907f1ed
Kevin Kao <kk415kk@gmail.com>
2014-12-29 01:01:08 -0800

    Added scripts controller

---
+++ api/controllers/DataScriptController.js
@@ -0,0 +1,11 @@
+/**
+ * DataScriptController
+ *
+ * @description :: Server-side logic for managing Datascripts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
