f9dbe17f23ed4fe84e77ccb0c4b616b552fbf46b
Kevin Kao <kk415kk@gmail.com>
2014-12-29 17:35:02 -0800

    Added new policies, fixed logo placement, WIP on data scripts

--- api/controllers/DataScriptController.js
+++ api/controllers/DataScriptController.js
@@ -5,7 +5,23 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var PATH_CONFIG = sails.config.paths;
+var DATA_DROP_PATH = PATH_CONFIG.DATASET_DROP_PATH;
+
 module.exports = {
-	
+  create: function(req, res) {
+    return res.redirect('/admin/manage_data_scripts');
+  },
+  destroy: function(req, res) {
+    return res.redirect('/admin/manage_data_scripts');
+  },
+
+  // BUILT-IN DATA MANAGEMENT TOOLS
+	script_archive: function(req, res) {
+    return res.redirect('/admin/manage_data_scripts');
+  },
+  script_move: function(req, res) {
+    return res.redirect('/admin/manage_data_scripts');
+  }
 };
 
