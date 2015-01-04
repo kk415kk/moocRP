87c4645b3d13e0067090d2c7764d59e00bbb9356
Kevin Kao <kk415kk@gmail.com>
2015-01-04 14:18:13 -0800

    WIP - data scriripts; added status updates

--- api/controllers/JobController.js
+++ api/controllers/JobController.js
@@ -6,15 +6,39 @@
  */
 
 var updateStartTime = function (jobID, startTime, cb) {
-
+  Job.findOne(jobID, function (err, job) {
+    if (err || !job) return cb(err);
+    params = { status: status };
+    job.startTime = startTime;
+    job.save(function (err2) {
+      if (err2) return cb(err2);
+      return cb();
+    });
+  });
 }
 
 var updateEndTime = function (jobID, endTime, cb) {
-
+  Job.findOne(jobID, function (err, job) {
+    if (err || !job) return cb(err);
+    params = { status: status };
+    job.endTime = endTime;
+    job.save(function (err2) {
+      if (err2) return cb(err2);W
+      return cb();
+    });
+  });
 }
 
 var updateStatus = function(jobID, status, cb) {
-
+  Job.findOne(jobID, function (err, job) {
+    if (err || !job) return cb(err);
+    params = { status: status };
+    job.status = status;
+    job.save(function (err2) {
+      if (err2) return cb(err2);
+      return cb();
+    });
+  });
 }
 
 module.exports = {
@@ -22,15 +46,16 @@ module.exports = {
     var params = req.params.all();
     Job.create(params, function (err, job) {
       if (err || !job) return res.json( {'success': false });
-
       return res.json({ 'success': true });
     });
   },
   destroy: function(req, res) {
-
+    Job.destroy(req.param['id'], function (err, job) {
+      return res.json({ 'success': true });
+    });
   },
   update: function(req, res) {
-    
+    // TODO
   }
 };
 
