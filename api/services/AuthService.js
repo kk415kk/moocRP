69ecf8d103f356501d0d4644a0c4f98ab7cab74e
Kevin Kao <kk415kk@gmail.com>
2015-03-11 22:14:54 -0700

    Edited auth service

--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -44,5 +44,5 @@ module.exports = {
     });
   },
 
-  serviceURL: settings.protocol + settings.appEnvMap[settings.environment]
+  serviceURL: settings.protocol + settings.appEnvMap[settings.environment] + settings.port
 }
